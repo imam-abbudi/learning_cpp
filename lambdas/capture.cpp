38cfb0d31b2dcc09b641a3dd866de4059f431205
abbudi <abbudi@abbudi.abbudi>
2026-09-05 19:05:43 +0300

    lambda capture clause shenanigans

---
+++ lambdas/capture.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+void lets_see(const auto &fn) {
+  int a = 5;
+
+  std::cout << fn(a) << '\n';
+}
+
+int main() {
+  {
+    int user_defined = 10;
+
+    // stores a copy of user_defined
+    auto lmd = [user_defined](int a) { return user_defined + a; };
+
+    std::cout << lmd(5) << '\n';
+
+    // 'lmd' object is still alive at this point
+    // no undefined behavior
+    lets_see(lmd);
+  }
+
+  {
+    int a = 10, b = 20;
+
+    // = captures by copy
+    // & captures by reference
+    auto lmd = [=]() { return a + b; };
+
+    std::cout << lmd() << '\n';
+  }
+}
